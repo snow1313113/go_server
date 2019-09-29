8211fb94f81c7cea024fa79beff73fa7cc61474b
snow1313113 <59796717@qq.com>
2019-09-29 09:49:37 +0800

    增加一个简单的build脚本和测试代码

---
+++ build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+if [ $# -lt 1 ]; then
+    echo "USAGE: $0 sub_dir"
+    echo "e.g.: $0 test"
+    exit -1
+fi
+
+cd `dirname $0`
+CURRENT_DIR=`pwd`
+export GOPATH=${CURRENT_DIR}
+cd src/$1
+go build .
