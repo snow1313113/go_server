c5c397dda8151034ba8fb3f5cfd5aceadbda7d9c
snow1313113 <59796717@qq.com>
2020-02-23 20:44:04 +0800

    完善log.go，输出更合理

--- src/utils/log.go
+++ src/utils/log.go
@@ -9,51 +9,56 @@ import (
 )
 
 // levels
-type logLevelType uint32
+type LogLevelType uint32
 const (
-    DebugLevel logLevelType = iota
+    DebugLevel LogLevelType = iota
     InfoLevel
     ErrorLevel
     FatalLevel
 )
 
 const (
-    printDebugLevel = "[debug ] "
-    printInfoLevel  = "[info  ] "
-    printErrorLevel = "[error ] "
-    printFatalLevel = "[fatal ] "
+    printDebugLevel = "|debug|"
+    printInfoLevel  = "|info|"
+    printErrorLevel = "|error|"
+    printFatalLevel = "|fatal|"
 )
 
 type Logger struct {
-    level      logLevelType
+    level      LogLevelType
     baseLogger *log.Logger
     baseFile   *os.File
 }
 
-func NewLogger(level logLevelType, pathname string) (*Logger, error) {
-    // logger
+func NewLogger(level LogLevelType, pathname string, prefix string) (*Logger, error) {
     var baseLogger *log.Logger
     var baseFile *os.File
     if pathname != "" {
         now := time.Now()
 
-        filename := fmt.Sprintf("%d%02d%02d_%02d_%02d_%02d.log",
-        now.Year(),
-        now.Month(),
-        now.Day(),
-        now.Hour(),
-        now.Minute(),
-        now.Second())
-
-        file, err := os.Create(path.Join(pathname, filename))
+        err := os.MkdirAll(pathname, os.ModeDir | os.ModePerm)
         if err != nil {
             return nil, err
         }
 
-        baseLogger = log.New(file, "", log.LstdFlags)
+        filename := fmt.Sprintf("%s_%d%02d%02d%02d.log",
+        prefix,
+        now.Year(),
+        now.Month(),
+        now.Day(),
+        now.Hour())
+
+        file, err := os.OpenFile(path.Join(pathname, filename), os.O_RDWR | os.O_CREATE | os.O_APPEND, 0644)
+        if err != nil {
+            return nil, err
+        }
+
+        pid_str := fmt.Sprintf("%d|", os.Getpid())
+
+        baseLogger = log.New(file, pid_str, log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
         baseFile = file
     } else {
-        baseLogger = log.New(os.Stdout, "", log.LstdFlags)
+        baseLogger = log.New(os.Stdout, "", log.LstdFlags | log.Lmicroseconds | log.Lshortfile)
     }
 
     logger := new(Logger)
@@ -74,7 +79,7 @@ func (logger *Logger) Close() {
     logger.baseFile = nil
 }
 
-func (logger *Logger) doPrintf(level logLevelType, printLevel string, format string, a ...interface{}) {
+func (logger *Logger) doPrintf(level LogLevelType, printLevel string, format string, a ...interface{}) {
     if level < logger.level {
         return
     }
@@ -83,12 +88,9 @@ func (logger *Logger) doPrintf(level logLevelType, printLevel string, format str
     }
 
     format = printLevel + format
+    // 这个call depth参数有点蛋疼，竟然要使用的人去算究竟调用栈有多少层
+    // 因为需要打印的位置是调用下面的Debug之类的函数的位置，所以最终到里面的runtime.Caller调用栈是三层
     logger.baseLogger.Output(3, fmt.Sprintf(format, a...))
-
-    // todo 这里直接退出？不好吧，后面再去掉吧
-    if level == FatalLevel {
-        os.Exit(1)
-    }
 }
 
 func (logger *Logger) Debug(format string, a ...interface{}) {
@@ -107,7 +109,7 @@ func (logger *Logger) Fatal(format string, a ...interface{}) {
     logger.doPrintf(FatalLevel, printFatalLevel, format, a...)
 }
 
-var gLogger, _ = NewLogger(DebugLevel, "")
+var gLogger, _ = NewLogger(DebugLevel, "", "__global__")
 
 // It's dangerous to call the method on logging
 func ExportLogger(logger *Logger) {
